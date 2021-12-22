41c40805d4f7d97d61a912c4a5bf50238a6a53a5
abbasymot <abbasymotdev@gmail.com>
2021-12-22 14:25:28 +0330

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -45,3 +45,19 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post", kwargs={'slug': self.slug})
+
+
+class Comment(models.Model):
+    STATUS_CHOICES = [
+        ('p', 'Publish'),
+        ('d', 'Draft')
+    ]
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField(max_length=2000)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='p')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
