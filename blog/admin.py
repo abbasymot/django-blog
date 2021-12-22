cd795d3635fb5d236b02023630ea0f5362b67eee
abbasymot <abbasymotdev@gmail.com>
2021-12-22 14:32:16 +0330

    CommentAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 # Register your models here.
 
 @admin.register(Post)
@@ -25,3 +25,7 @@ class TagAdmin(admin.ModelAdmin):
     search_fields = ['title']
     prepopulated_fields = {"slug" : ("title",)}
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post","profile", "status")
